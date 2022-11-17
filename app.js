82287abaafab36c541be78950ef2006d9ff13b7b
Kenni Woodard <kennikristine@gmail.com>
2022-11-16 19:35:44 -0800

    sign up functional

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-import { redirectIfLoggedIn, signInUser } from './fetch-utils.js';
+import { redirectIfLoggedIn, signInUser, signupUser } from './fetch-utils.js';
 
 const signInForm = document.getElementById('sign-in');
 const signInEmail = document.getElementById('sign-in-email');
 const signInPassword = document.getElementById('sign-in-password');
 
 const signUpForm = document.getElementById('sign-up');
-
+const signUpEmail = document.getElementById('sign-up-email');
+const signUpPassword = document.getElementById('sign-up-password');
 // Wire up sign in and sign up forms to supabase
 
 signInForm.addEventListener('submit', async (e) => {
@@ -18,6 +19,12 @@ signInForm.addEventListener('submit', async (e) => {
     } else {
         console.error(user);
     }
+});
+
+signUpForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    await signupUser(signUpEmail.value, signUpPassword.value);
 });
 
 // Redirect to /other-page on successful auth
--- fetch-utils.js
+++ fetch-utils.js
@@ -9,7 +9,11 @@ export function getUser() {
     return client.auth.session() && client.auth.session().user;
 }
 
-export async function signupUser(email, password) {}
+export async function signupUser(email, password) {
+    const response = await client.auth.signUp({ email, password });
+
+    return response.user;
+}
 
 export async function signInUser(email, password) {}
 
